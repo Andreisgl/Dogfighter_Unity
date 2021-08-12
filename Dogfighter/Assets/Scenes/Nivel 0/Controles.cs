5e403d4dda62bad0670d6a494189e2c81aab84f3
Andrei Segal <andreisegal245@gmail.com>
2021-08-12 00:30:38 -0300

    add resumo da classe

--- Dogfighter/Assets/Scenes/Nivel 0/Controles.cs
+++ Dogfighter/Assets/Scenes/Nivel 0/Controles.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 namespace Aeronave
 {
 
-
+/*
+Essa classe atua em conjunto com a classe "MetodoControle".
+Ela recebe as entradas dos comandos da aeronave, os fatores,
+os passa para "MetodoControle" para cálculo
+e aplica recebe a saída para aplicar em movimentos na da aeronave.
+*/
 
 public class Controles : MonoBehaviour
 {
