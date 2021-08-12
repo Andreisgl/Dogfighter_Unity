7751f9ad2157ad50f20c5d49ba4a36f315e84715
Andrei Segal <andreisegal245@gmail.com>
2021-08-12 00:31:45 -0300

    remover RigidBody desnecessário

--- Dogfighter/Assets/Scenes/Nivel 0/MetodoControle.cs
+++ Dogfighter/Assets/Scenes/Nivel 0/MetodoControle.cs
@@ -13,12 +13,12 @@ namespace Aeronave
 
     public class MetodoControle : MonoBehaviour
     {
-        Rigidbody aero_rb;  //Cria um objeto RigidBody
+        
 
         // Start is called before the first frame update
         void Start()
         {
-            aero_rb = GetComponent<Rigidbody>();    //Considera o objeto atual o Rigidbody
+            
         }
 
         // Update is called once per frame
