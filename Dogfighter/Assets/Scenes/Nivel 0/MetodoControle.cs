201abae2606dccb9c4d179d93fd0bd1509bffd64
Andrei Segal <andreisegal245@gmail.com>
2021-08-11 23:28:44 -0300

    criei script de metodo de controle

---
+++ Dogfighter/Assets/Scenes/Nivel 0/MetodoControle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetodoControle : MonoBehaviour
+{
+    Rigidbody aero_rb;  //Cria um objeto RigidBody
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aero_rb = GetComponent<Rigidbody>();    //Considera o objeto atual o Rigidbody
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
