44869741727f03116b192e313a04f7dc851effe2
Andrei Segal <andreisegal245@gmail.com>
2021-08-11 23:34:00 -0300

    correção de sintaxe

--- Dogfighter/Assets/Scenes/Nivel 0/MetodoControle.cs
+++ Dogfighter/Assets/Scenes/Nivel 0/MetodoControle.cs
@@ -2,27 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-Namespace Aeronave
+namespace Aeronave
 {
 
 
 
-public class MetodoControle : MonoBehaviour
-{
-    Rigidbody aero_rb;  //Cria um objeto RigidBody
-
-    // Start is called before the first frame update
-    void Start()
+    public class MetodoControle : MonoBehaviour
     {
-        aero_rb = GetComponent<Rigidbody>();    //Considera o objeto atual o Rigidbody
-    }
+        Rigidbody aero_rb;  //Cria um objeto RigidBody
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+            aero_rb = GetComponent<Rigidbody>();    //Considera o objeto atual o Rigidbody
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        void FixedUpdate()
+        {
+
+        }
     }
-}
 
 
 
