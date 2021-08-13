eff18a9f6b7b7f17480bd68cbe4eda140d1c492c
Andrei Segal <andreisegal245@gmail.com>
2021-08-13 11:24:39 -0300

    remove euler calculations

--- Dogfighter/Assets/Scenes/Nivel 0/FisicaLocal.cs
+++ Dogfighter/Assets/Scenes/Nivel 0/FisicaLocal.cs
@@ -18,9 +18,6 @@ namespace Aeronave
     {
         Rigidbody aero_rb;  //Cria um objeto RigidBody
 
-        Vector3 velLocal;   //Cria vetor de velocidade local.
-
-        float anguloAtqVert;
 
         // Start is called before the first frame update
         void Start()
@@ -36,44 +33,17 @@ namespace Aeronave
 
         void FixedUpdate()
         {
-            calculaVelLocal();
-            Debug.Log("Aoa Vert: " + calculaAngAtqVert()  +  "  Aoa Hor: " + calculaAngAtqHor() );
-            //Debug.Log(velLocal);
+
         }
 
 
 
 
-        void calculaVelLocal()
-        {
-            velLocal = Vector3.Normalize( transform.InverseTransformDirection(aero_rb.velocity) );
-            
-        }
+      
 
 
         //Métodos de saída:
-        float calculaAngAtqVert()
-        {
-            Vector2 vetYZ; //Cria vetor bidimensional cujas componentes representam um plano local YZ. A partir dele será calculado o ângulo de ataque
-
-            vetYZ = new Vector2(velLocal.y, velLocal.z);    //Adiciona os valores de velocidade local ao vetor.
-
-            if(velLocal.y < 0)  //Se o nariz aponta para cima:
-                return Vector2.Angle( vetYZ, transform.forward );
-            else    //Se está neutro/aponta para baixo:
-                return -Vector2.Angle( -vetYZ, transform.forward );
-        }
-        float calculaAngAtqHor()
-        {
-            Vector2 vetYZ; //Cria vetor bidimensional cujas componentes representam um plano local YZ. A partir dele será calculado o ângulo de ataque
-
-            vetYZ = new Vector2(velLocal.x, velLocal.z);    //Adiciona os valores de velocidade local ao vetor.
-
-            if(velLocal.y < 0)  //Se o nariz aponta para cima:
-                return Vector2.Angle( vetYZ, transform.forward );
-            else    //Se está neutro/aponta para baixo:
-                return -Vector2.Angle( -vetYZ, transform.forward );
-        }
+     
 
 
 
