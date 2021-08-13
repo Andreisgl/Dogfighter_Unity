f53c3843a374a959858aa9d3b3c37dae7650ef1f
Andrei Segal <andreisegal245@gmail.com>
2021-08-12 23:57:35 -0300

    add namespace

--- Dogfighter/Assets/Scenes/Nivel 0/FisicaLocal.cs
+++ Dogfighter/Assets/Scenes/Nivel 0/FisicaLocal.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FisicaLocal : MonoBehaviour
+namespace Aeronave
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+
+    public class FisicaLocal : MonoBehaviour
     {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
