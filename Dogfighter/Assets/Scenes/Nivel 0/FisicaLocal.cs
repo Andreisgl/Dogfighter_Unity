9022927b254ab4baca896c0bf971781c997cf156
Andrei Segal <andreisegal245@gmail.com>
2021-08-13 00:04:53 -0300

    add definições de rigidbody

--- Dogfighter/Assets/Scenes/Nivel 0/FisicaLocal.cs
+++ Dogfighter/Assets/Scenes/Nivel 0/FisicaLocal.cs
@@ -16,10 +16,12 @@ namespace Aeronave
 
     public class FisicaLocal : MonoBehaviour
     {
+        Rigidbody aero_rb;  //Cria um objeto RigidBody
+
         // Start is called before the first frame update
         void Start()
         {
-            
+            aero_rb = GetComponent<Rigidbody>();    //Considera o objeto atual o Rigidbody
         }
 
         // Update is called once per frame
