b7e4e0462647e9a22c06fcb5ac18dc6c17801d09
Andrei Segal <andreisegal245@gmail.com>
2021-08-21 02:26:46 -0300

    add namespace

--- Dogfighter/Assets/Scenes/Nivel 0/fisica_local/CL_AoA.cs
+++ Dogfighter/Assets/Scenes/Nivel 0/fisica_local/CL_AoA.cs
@@ -2,49 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CL_AoA : MonoBehaviour
+
+namespace Aeronave
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    public class CL_AoA : MonoBehaviour
     {
-        start_empuxoAsa();  //Inicializa os valores da curva
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void FixedUpdate()
-    {
-
-    }
-
-
-    public AnimationCurve curva;
-
-
-    void start_empuxoAsa()
+        // Start is called before the first frame update
+        void Start()
         {
-            curva.AddKey(-90f, 0f);
+            start_empuxoAsa();  //Inicializa os valores da curva
+        }
 
-            //AoA positivo
-            curva.AddKey(0f, 0f);
-            curva.AddKey(15f, 1.2f);
-            curva.AddKey(25f, 1.6f);
-            curva.AddKey(35f, 1.9f);
-            
-            curva.AddKey(90f, 0f);
-
-            //AoA negativo
-            
-            curva.AddKey(-15f, -1.2f);
-            curva.AddKey(-25f, -1.6f);
-            curva.AddKey(-35f, -1.9f);
-            
-            curva.AddKey(-90f, 0f);
-            
+        // Update is called once per frame
+        void Update()
+        {
             
         }
+
+        void FixedUpdate()
+        {
+
+        }
+
+
+        public AnimationCurve curva;
+
+
+        void start_empuxoAsa()
+            {
+                curva.AddKey(-90f, 0f);
+
+                //AoA positivo
+                curva.AddKey(0f, 0f);
+                curva.AddKey(15f, 1.2f);
+                curva.AddKey(25f, 1.6f);
+                curva.AddKey(35f, 1.9f);
+                
+                curva.AddKey(90f, 0f);
+
+                //AoA negativo
+                
+                curva.AddKey(-15f, -1.2f);
+                curva.AddKey(-25f, -1.6f);
+                curva.AddKey(-35f, -1.9f);
+                
+                curva.AddKey(-90f, 0f);
+                
+                
+            }
+    }
+
+
 }
