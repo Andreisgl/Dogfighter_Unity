8467a8a299d3f4ba14fe1a5793e6b17358c37f2c
Andrei Segal <andreisegal245@gmail.com>
2021-08-22 03:39:29 -0300

    add class summary

--- Dogfighter/Assets/Scenes/Nivel 0/fisica_local/Lift_Force.cs
+++ Dogfighter/Assets/Scenes/Nivel 0/fisica_local/Lift_Force.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 namespace Aeronave
 {
 
-    /**/
+    /*
+        Essa classe aplica a força de Empuxo (Lift) das asas baseado em:
+        -Ângulo de Ataque(AoA): Calculado na classe AoA_Calc.
+        -Coeficiente de Empuxo(CL): Calculado na classe CL_AoA.
+    */
 
     public class Lift_Force : MonoBehaviour
     {
