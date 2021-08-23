64fa3986a3a0b30f1e6a9a5a39f2c1ad948d5b96
Andrei Segal <andreisegal245@gmail.com>
2021-08-23 01:18:44 -0300

    working on debug

--- Dogfighter/Assets/Scenes/Nivel 0/fisica_local/Drag.cs
+++ Dogfighter/Assets/Scenes/Nivel 0/fisica_local/Drag.cs
@@ -64,7 +64,7 @@ namespace Aeronave
 
         void FixedUpdate()
         {
-
+            //Debug.Log( curvaAreaZX.Evaluate( ge ) );
         }
 
         public AnimationCurve curvaAreaZX;  //Indicará a interpolação dos valores de área ao girarem no eixo Y (Horizontal). (Frente e trás, cima e baixo)
