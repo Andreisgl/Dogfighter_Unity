1a830b9cafaaee8395a000b5ea50b27730bbbe6a
Andrei Segal <andreisegal245@gmail.com>
2021-08-23 00:22:49 -0300

    update arrays for drag calculation

--- Dogfighter/Assets/Scenes/Nivel 0/fisica_local/Drag.cs
+++ Dogfighter/Assets/Scenes/Nivel 0/fisica_local/Drag.cs
@@ -21,6 +21,8 @@ namespace Aeronave
         float vel = 0f; //Velocidade do ar.  (m/s - Metros por segundo)
         float[] areaCorpo = new float[6]; //Área de superfície do corpo do avião em todas as 6 direções!  (m^2 - Metro quadrado)
                                           //0: Frente, 1: Trás, 2: Direita, 3: Esquerda, 4: Cima, 5: Baixo
+        float[] CDCorpo = new float[6];   //Coeficiente de arrasto do corpo do avião em todas as 6 direções!  (m^2 - Metro quadrado)
+                                          //0: Frente, 1: Trás, 2: Direita, 3: Esquerda, 4: Cima, 5: Baixo
         float CD = 0f;  //Coeficiente de arrasto do corpo.  (Unidade sem dimensão)
 
 
@@ -32,10 +34,23 @@ namespace Aeronave
             //ATENÇÃO!!! - TODO - Essas definições são provisórias. Esses dados serão recuperados de outras classes depois!
             densAr = 1.201f;
             //vel = 0f;
-            //areaCorpo;
+                //Define provisoriamente os valores do vetor de áreas do corpo.
+                areaCorpo[0] = ;
+                areaCorpo[1] = ;
+                areaCorpo[2] = 1.5f * 28f;  //1,5x a área das asas, no olhômetro
+                areaCorpo[3] = 1.5f * 28f;  //1,5x a área das asas, no olhômetro
+                areaCorpo[4] = 3f * 28f;    //3x a área das asas, no olhômetro
+                areaCorpo[5] = 3f * 28f;    //3x a área das asas, no olhômetro
+                
+                //Define provisoriamente os valores do vetor de CD do corpo.
+                areaCorpo[0] = 0.0175f; //Valor obtido na internet
+                areaCorpo[1] = 0.0175f; //Coloquei o mesmo do outro por preguiça
+                areaCorpo[2] = 1.98f;   //Valor de uma chapa paralela ao fluxo.
+                areaCorpo[3] = 1.98f;   //Valor de uma chapa paralela ao fluxo.
+                areaCorpo[4] = 1.98f;   //Valor de uma chapa paralela ao fluxo.
+                areaCorpo[5] = 1.98f;   //Valor de uma chapa paralela ao fluxo.
+            
             //CD = 0f;
-
-            atualizaAreasCorpo();
         }
 
         
