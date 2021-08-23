4fd0585e2f3ec0f9988022730f201617f7d4c5b4
Andrei Segal <andreisegal245@gmail.com>
2021-08-22 23:39:40 -0300

    add summary

--- Dogfighter/Assets/Scenes/Nivel 0/fisica_local/Drag.cs
+++ Dogfighter/Assets/Scenes/Nivel 0/fisica_local/Drag.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 namespace Aeronave
 {
 
-
+    /*
+        Essa classe calcula a força de Arrasto (Drag) de acordo com a orientação da aeronave relativa ao vetor de velocidade local
+    */
 
     public class Drag : MonoBehaviour
     {
