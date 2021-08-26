ac650b4c0e106b2ccae1d5a67f69eb42d0703135
Andrei Segal <andreisegal245@gmail.com>
2021-08-25 22:02:12 -0300

    zerar arrasto frontal

--- Dogfighter/Assets/Scenes/Nivel 0/fisica_local/Drag.cs
+++ Dogfighter/Assets/Scenes/Nivel 0/fisica_local/Drag.cs
@@ -41,8 +41,8 @@ namespace Aeronave
             densAr = 1.201f;
             //vel = 0f;
                 //Define provisoriamente os valores do vetor de áreas do corpo.
-                areaCorpo[0] = 1f * 1f;  //0,25x a área das asas, no olhômetro    
-                areaCorpo[1] = 1f * 1f;  //0,25x a área das asas, no olhômetro  
+                areaCorpo[0] = 0f * 1f;  //0,25x a área das asas, no olhômetro    
+                areaCorpo[1] = 0f * 1f;  //0,25x a área das asas, no olhômetro  
                 areaCorpo[2] = 1f * 1f;  //2x a área das asas, no olhômetro
                 areaCorpo[3] = 1f * 1f;  //2x a área das asas, no olhômetro
                 areaCorpo[4] = 1f * 1f;    //3x a área das asas, no olhômetro
