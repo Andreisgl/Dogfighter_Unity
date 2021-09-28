8488aef2b455db060d020a0570969fd4ac4ce715
Andrei Segal <andreisegal245@gmail.com>
2021-09-28 01:40:12 -0300

    add sumário

--- Dogfighter/Assets/Scripts_Aeronave/AeroControles.cs
+++ Dogfighter/Assets/Scripts_Aeronave/AeroControles.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+    Essa classe aplica os controles básicos da aeronave.
+*/
 public class AeroControles : MonoBehaviour
 {
     void Start()
@@ -19,5 +22,8 @@ public class AeroControles : MonoBehaviour
 
     }
 
-    
+    void atualizaControles()    //Essa função lê e atualiza o valor dos controles
+    {
+
+    }
 }
