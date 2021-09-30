1e56615f257594c4b5b23bba67b7a54beba854a1
Andrei Segal <andreisegal245@gmail.com>
2021-09-30 00:02:37 -0300

    primeiro teste da classe

--- Dogfighter/Assets/Scripts_Aeronave/AoA_Calc.cs
+++ Dogfighter/Assets/Scripts_Aeronave/AoA_Calc.cs
@@ -6,16 +6,28 @@ public class AoA_Calc : MonoBehaviour
 {
     Rigidbody aero_rb;  //Cria um objeto RigidBody.
 
-    // Start is called before the first frame update
+    //Variáveis de física
+        private Vector3 velLocal;   //Vetor que indica a Velocidade Local da aeronave.
+
     void Start()
     {
         aero_rb = GetComponent<Rigidbody>();    //aero_rb agora é o Rigidbody da aeronave atual.
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        calculaFisica();
+        Debug.Log("velLocal : " + velLocal);
+    }
+
+    void calculaFisica()
+    {
+        velLocal = Vector3.Normalize( transform.InverseTransformDirection(aero_rb.velocity) );  //Atualiza o vetor global velLocal.
     }
 }
