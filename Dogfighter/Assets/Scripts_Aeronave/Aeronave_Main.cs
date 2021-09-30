5d15eb902189c3e078fd9a07aaed3ca8c858a8b7
Andrei Segal <andreisegal245@gmail.com>
2021-09-30 02:17:09 -0300

    remover rigidbody

--- Dogfighter/Assets/Scripts_Aeronave/Aeronave_Main.cs
+++ Dogfighter/Assets/Scripts_Aeronave/Aeronave_Main.cs
@@ -7,11 +7,11 @@ Essa classe reúne dados e estados provenientes de outras classes para acesso ma
 */
 public class Aeronave_Main : MonoBehaviour
 {
-    Rigidbody aero_rb;  //Cria um objeto RigidBody.
+    
 
     void Start()
     {
-        aero_rb = GetComponent<Rigidbody>();    //aero_rb agora é o Rigidbody da aeronave atual.
+        
     }
 
     void Update()
