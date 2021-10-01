9845548a1b3a6c488cba9fe1b96f3cd0e35cce9e
Andrei Segal <andreisegal245@gmail.com>
2021-09-30 21:02:42 -0300

    renomeei função

--- Dogfighter/Assets/Scripts_Aeronave/Drag.cs
+++ Dogfighter/Assets/Scripts_Aeronave/Drag.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+Essa classe aplica arrasto normal e arrasto induzido de acordo com seus cálculos.
+*/
 public class Drag : MonoBehaviour
 {
     Rigidbody aero_rb;  //Cria um objeto RigidBody.
     
     //Variáveis de física
         private Vector3 velGlobal;   //Vetor que indica a direção e intensidade da Velocidade Local da aeronave.
+        float valorArrasto; //Valor da força de arrasto (Drag) a ser aplicada em Newtons.
     void Start()
     {
         aero_rb = GetComponent<Rigidbody>();    //aero_rb agora é o Rigidbody da aeronave atual.
@@ -25,7 +29,7 @@ public class Drag : MonoBehaviour
         //Debug.Log(velGlobal);
     }
 
-    void calculaIndDrag()
+    void calculaArrInd()    //Calcula o Arrasto Induzido da aeronave.
     {
         
     }
