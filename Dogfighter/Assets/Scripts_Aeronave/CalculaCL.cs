fba3f7fe9b34c6b4019fae47d065c4bb917e4c0e
Andrei Segal <andreisegal245@gmail.com>
2021-09-30 21:04:33 -0300

    criei classe CalculaCL

---
+++ Dogfighter/Assets/Scripts_Aeronave/CalculaCL.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculaCL : MonoBehaviour
+{
+    /*
+    Essa classe calcula o Coeficiente de Empuxo(CL) a partir do ângulo de ataque.
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
