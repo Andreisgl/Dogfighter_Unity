f9cc01b807d3561430ec5f7503c756902211f76b
Andrei Segal <andreisegal245@gmail.com>
2021-09-30 21:10:21 -0300

    add funções

--- Dogfighter/Assets/Scripts_Aeronave/CalculaCL.cs
+++ Dogfighter/Assets/Scripts_Aeronave/CalculaCL.cs
@@ -7,15 +7,42 @@ public class CalculaCL : MonoBehaviour
     /*
     Essa classe calcula o Coeficiente de Empuxo(CL) a partir do ângulo de ataque.
     */
-    // Start is called before the first frame update
+    [SerializeField]
+    AnimationCurve curva;   //Inicia a curva de CL.
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    
+    void iniciaCurvaCL()    //Inicia os valores da curva de CL. Os valores de CL variam de acordo com o Ângulo de Ataque (AoA)
+    {
+        //AoA positivo
+        curva.AddKey(0f, 0f);
+        curva.AddKey(15f, 1.2f);
+        curva.AddKey(25f, 1.6f);
+        curva.AddKey(35f, 1.9f);
+        
+        curva.AddKey(45f, 0f);
+
+        //AoA negativo
+        
+        curva.AddKey(-15f, -1.2f);
+        curva.AddKey(-25f, -1.6f);
+        curva.AddKey(-35f, -1.9f);
+        
+        curva.AddKey(-45f, 0f);
     }
 }
