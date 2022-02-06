0d272b7a30351e410831da1e907100575833c930
Andrei Segal <andreisegal245@gmail.com>
2022-02-06 16:18:31 -0300

    create class summary

--- Dogfighter/Assets/Scripts_Aeronave/AircraftPlayerControl.cs
+++ Dogfighter/Assets/Scripts_Aeronave/AircraftPlayerControl.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+This class unifies "Stick"(Manche) and "Engine"(Motor) under a single script.
+Now, this script will get player input, intead of those previous classes
+*/
 public class AircraftPlayerControl : MonoBehaviour
 {
     // Start is called before the first frame update
